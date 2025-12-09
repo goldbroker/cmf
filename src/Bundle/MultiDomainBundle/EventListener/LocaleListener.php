fcc94f05d976f0554a949957dd978e83f2330274
Pierre-Olivier Huguet <pohuguet@goldbroker.com>
2025-12-09 15:01:34 +0100

    fix MASTER_REQUEST in mltidomain (#23)

--- src/Bundle/MultiDomainBundle/EventListener/LocaleListener.php
+++ src/Bundle/MultiDomainBundle/EventListener/LocaleListener.php
@@ -19,7 +19,7 @@ class LocaleListener
 
     public function onKernelRequest(RequestEvent $event)
     {
-        if (HttpKernelInterface::MASTER_REQUEST !== $event->getRequestType()) {
+        if (HttpKernelInterface::MAIN_REQUEST !== $event->getRequestType()) {
             return;
         }
 
